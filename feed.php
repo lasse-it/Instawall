fd34affd1c7571f803d506482a3bc7b727361858
Lasse Hyldahl Jensen <lasse@lasse-it.dk>
2015-11-20 23:49:16 +0100

    Fixed "Use of undefined constant"

--- feed.php
+++ feed.php
@@ -1,6 +1,6 @@
 <?PHP
     include 'config.php';
-	$tag = $_GET[tag];
+	$tag = $_GET['tag'];
 	$input = file_get_contents("https://api.instagram.com/v1/tags/{$tag}/media/recent?access_token={$access_token}");
 	$edit = json_decode($input, TRUE);
 	$output = $edit['data'];
